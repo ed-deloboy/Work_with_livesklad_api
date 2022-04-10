ea84605137cdac5112bce7f94d593aab55e1bd8a
Ed Bondarenko <edbondarenko@Eds-MacBook-Air.local>
2022-04-10 21:04:18 +0300

    commit init

---
+++ config/connect.php
@@ -0,0 +1,54 @@
+<!-- https://api.livesklad.com/auth -->
+
+<?php
+
+// получение токена через пост
+// $url = 'https://api.livesklad.com/auth';
+// $params = array(
+//     'login' => 'ed5e42cb8adb9aa38aef6de3861b2309', // в http://localhost/post.php это будет $_POST['param1'] == '123'
+//     'password' => 'VVVGUnnBx8ZJStU1qn4m', // в http://localhost/post.php это будет $_POST['param2'] == 'abc'
+// );
+// $result = file_get_contents($url, false, stream_context_create(array(
+//     'http' => array(
+//         'method'  => 'POST',
+//         'header'  => 'Content-type: application/x-www-form-urlencoded',
+//         'content' => http_build_query($params)
+//     )
+// )));
+
+// $ansver_arr = json_decode($result, true);
+
+// echo '<pre>';
+
+// print_r($ansver_arr);
+
+// echo $result;
+
+
+
+
+
+
+
+$token = 'MB3vN6aFM8hPAsfuP6oTpVVfGmQRjkLhlXWsRqkyjNK7JRT47tii30oVE6B2g08r';
+
+
+// https://api.vk.com/method/friends.getSuggestions?&access_token=&v=5.120
+
+// $login = 'ed5e42cb8adb9aa38aef6de3861b2309';
+// $pass = 'VVVGUnnBx8ZJStU1qn4m';
+
+// $auth = 'https://api.livesklad.com/auth?Body&login='.$login.'&password='.$pass;
+
+// $response_json = file_get_contents($auth, true);
+// $ansver_arr = json_decode($response_json, true);
+// echo "hello livskld";
+// echo "<br>";
+
+
+
+
+
+
+
+?>
