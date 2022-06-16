c73c889492ade42eef57387393ccafbe254384bf
Ed Bondarenko <edbondarenko@Eds-MacBook-Air.local>
2022-06-16 21:35:40 +0300

    Fix add to DB Good

--- config/get_dataAgent_name.php
+++ config/get_dataAgent_name.php
@@ -2,12 +2,11 @@
 require_once 'get_token.php';
 
 echo '<pre>';
-
+// echo 777;
 $agent_name = trim($_POST['agent_name']);
 
-
 $params_agents = array(
-    'sort' => 'id ASC',
+    'sort' => 'id DESC',
     'pageSize' => '50',
     'page' => '1',
     'isBuyer' => true,
@@ -25,15 +24,22 @@ $result_agent = file_get_contents("https://api.livesklad.com/counteragents/", fa
 )));
 
 $data = json_decode($result_agent, true);
+
+print_r($data);
+exit();
+
 $ls_users = $data['data'];
 $users_for_db = array();
 echo $data['remainRequest'] . '<br>';
 
-// print_r(json_decode($result_agent, true));
+$db_user_id = mysqli_fetch_all(mysqli_query($conn, "SELECT user_id FROM users ORDER BY id DESC LIMIT 50"));
+$db_user_id = array_reverse($db_user_id);
 
-$db_user_id = mysqli_fetch_all(mysqli_query($conn, "SELECT user_id FROM users ORDER BY 'user_id' DESC LIMIT 50"));
 for ($i = 0; $i < 50; $i++) {
     $position = 0;
+
+    echo $ls_users[$i]['id'] . '<br>';
+    echo $db_user_id[$i][0] . '<br>';
 
     if ($ls_users[$i]['id'] != $db_user_id[$i][0]) {
 
@@ -45,17 +51,14 @@ for ($i = 0; $i < 50; $i++) {
         $ls_user_name = $ls_users[$i]['name'];
         $ls_user_phone = $ls_users[$i]['phones'][0];
         $ls_user_mail = $ls_users[$i]['email'];
-        
+
         mysqli_query($conn, "INSERT INTO `users`(`id`, `user_id`, `user_category`, `user_name`, `user_phone`, `user_mail`) VALUES (NULL,'$ls_user_id','$ls_user_category','$ls_user_name','$ls_user_phone','$ls_user_mail')");
     } else {
+        echo 'ID совпали' . '<br>';
+        echo 'Пропускаем ' . $ls_users[$i]['id'] . '<br>';
         $position = 1;
         if ($position == 1) {
             continue;
         }
     }
 }
-
-// for ($i = 0; $i < count($users_for_db); $i++) {
-//     echo $i . '<br>';
-//     echo $users_for_db[$i] . '<br>';
-// }
